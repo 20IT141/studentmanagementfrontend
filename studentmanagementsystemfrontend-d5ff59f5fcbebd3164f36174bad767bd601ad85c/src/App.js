674508acfb6385ac0ca3089d2c869121b778ef39
20IT141 <20it141@charusat.edu.in>
2022-09-12 11:39:28 +0530

    Added Navigationbar

--- studentmanagementsystemfrontend-d5ff59f5fcbebd3164f36174bad767bd601ad85c/src/App.js
+++ studentmanagementsystemfrontend-d5ff59f5fcbebd3164f36174bad767bd601ad85c/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
-  return (
-      <div className="App">
-        <Student/>
-        <StudentList/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <NavigationBar/>
+            <Student/>
+            <StudentList/>
+        </div>
+    );
 }
 
 export default App;
